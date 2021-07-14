422afc8f369b5300946942522907015e3060ab73
Gustavo Costa <cmtehenz@gmail.com>
2021-07-14 20:57:45 -0300

    :tada: _fnalizada

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const senhaChamada = document.querySelector('.senha');
 const caixaChamado = document.querySelector('.caixa');
+const minhaSenha = document.querySelector('#minhaSenha');
 
 const btSenhaComum  = document.querySelector('#btComum');
 const btSenhaRapido = document.querySelector('#btRapido');
@@ -22,32 +23,94 @@ function adicionarFila(event){
   if(event.target.id === 'btComum'){
     filaC++;
     filaComum.push(`C - ${filaC}`);
+    minhaSenha.innerHTML = `C - ${filaC}`;
   }else if(event.target.id === 'btRapido'){
     filaR++;
     filaRapida.push(`R - ${filaR}`);
+    minhaSenha.innerHTML = `R - ${filaR}`;
   }else if(event.target.id === 'btPrioritario'){
     filaP++;
-    filaPrioritaria.push(`P - ${filaP}`)
+    filaPrioritaria.push(`P - ${filaP}`);
+    minhaSenha.innerHTML = `P - ${filaP}`;
   }
 }
 
 function sairFila(event){
   if(event.target.id === 'btCaixa1'){
-    //atender prioritario
-    const senha = filaPrioritaria.shift();
-    senhaChamada.innerHTML = senha;
-    caixaChamado.innerHTML = 'Caixa 1';
-    if(filaPrioritaria.length === 0){
-      //atender rapido
-      const senha = filaRapida.shift();
+    if(filaPrioritaria.length > 0 ){
+      //atender prioritário
+      const senha = filaPrioritaria.shift();
       senhaChamada.innerHTML = senha;
       caixaChamado.innerHTML = 'Caixa 1';
-      if(filaRapida.length === 0){
-        //atender comum
-        const senha = filaComum.shift();
-        senhaChamada.innerHTML = senha;
-        caixaChamado.innerHTML = 'Caixa 1';
-      }
+    }else if(filaRapida.length > 0){
+      //atender Rapido
+      const senhaR = filaRapida.shift();
+      senhaChamada.innerHTML = senhaR;
+      caixaChamado.innerHTML = 'Caixa 1';
+    }else if(filaComum.length > 0){
+      //atender Comum
+      const senhaC = filaComum.shift();
+      senhaChamada.innerHTML = senhaC;
+      caixaChamado.innerHTML = 'Caixa 1';
+    }
+  }
+
+  if(event.target.id === 'btCaixa2'){
+    if(filaRapida.length > 0 ){
+      //atender Rapido
+      const senhaR = filaRapida.shift();
+      senhaChamada.innerHTML = senhaR;
+      caixaChamado.innerHTML = 'Caixa 2';
+    }else if(filaPrioritaria.length > 0){
+      //atender Prioritário
+      const senhaP = filaPrioritaria.shift();
+      senhaChamada.innerHTML = senhaP;
+      caixaChamado.innerHTML = 'Caixa 2';
+    }else if(filaComum.length > 0){
+      //atender Comum
+      const senhaC = filaComum.shift();
+      senhaChamada.innerHTML = senhaC;
+      caixaChamado.innerHTML = 'Caixa 2';
+    }
+  }
+
+  if(event.target.id === 'btCaixa3'){
+    if(filaRapida.length > 0 ){
+      //atender Rapido
+      const senhaR = filaRapida.shift();
+      senhaChamada.innerHTML = senhaR;
+      caixaChamado.innerHTML = 'Caixa 3';
+
+    }else if(filaPrioritaria.length > 0){
+      //atender Prioritário
+      const senhaP = filaPrioritaria.shift();
+      senhaChamada.innerHTML = senhaP;
+      caixaChamado.innerHTML = 'Caixa 3';
+    }else if(filaComum.length > 0){
+      //atender Comum
+      const senhaC = filaComum.shift();
+      senhaChamada.innerHTML = senhaC;
+      caixaChamado.innerHTML = 'Caixa 3';
+    }
+  }
+
+  if(event.target.id === 'btCaixa4'){
+    if(filaComum.length > 0 ){
+      //atender comum
+      const senha = filaComum.shift();
+      senhaChamada.innerHTML = senha;
+      caixaChamado.innerHTML = 'Caixa 4';
+
+    }else if(filaPrioritaria.length > 0){
+      //atender Prioritário
+      const senhaP = filaPrioritaria.shift();
+      senhaChamada.innerHTML = senhaP;
+      caixaChamado.innerHTML = 'Caixa 4';
+    }else if(filaRapida.length > 0){
+      //atender Rapido
+      const senhaR = filaRapida.shift();
+      senhaChamada.innerHTML = senhaR;
+      caixaChamado.innerHTML = 'Caixa 4';
     }
   }
 }
@@ -67,6 +130,18 @@ btSenhaPrioritario.addEventListener("click", (event) => {
 });
 
 btCaixa1.addEventListener("click", (event) => {
+  sairFila(event);
+});
+
+btCaixa2.addEventListener("click", (event) => {
+  sairFila(event);
+});
+
+btCaixa3.addEventListener("click", (event) => {
+  sairFila(event);
+});
+
+btCaixa4.addEventListener("click", (event) => {
   sairFila(event);
 });
 
